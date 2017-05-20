c1d17c4689b6550773faf985460442284c0164b5
keavlar <keavlar@gmail.com>
2017-05-20 12:59:05 +0300

    Add files via upload

---
+++ Java Script/global.js
@@ -0,0 +1,23 @@
+﻿(function () {
+
+    "use strict";
+    $(function () {
+
+
+
+    window.entites = {};
+
+    window.entites.urlObjs = {
+
+        brazil: "http://api.wunderground.com/api/c6a9124b70fbd2d2/geolookup/conditions/forecast/q/Brazil/Sao_Paulo.json",
+        china: "http://api.wunderground.com/api/c6a9124b70fbd2d2/geolookup/conditions/forecast/q/China/Hong_Kong.json",
+        germany: "http://api.wunderground.com/api/c6a9124b70fbd2d2/geolookup/conditions/forecast/q/Germany/Schonefeld.json",
+        london: "http://api.wunderground.com/api/c6a9124b70fbd2d2/geolookup/conditions/forecast/q/United_Kingdom/London.json",
+        newYork: "http://api.wunderground.com/api/c6a9124b70fbd2d2/geolookup/conditions/forecast/q/NY/New_York.json",
+        paris: "http://api.wunderground.com/api/c6a9124b70fbd2d2/geolookup/conditions/forecast/q/France/Paris.json",
+        switzerland: "http://api.wunderground.com/api/c6a9124b70fbd2d2/geolookup/conditions/forecast/q/Switzerland/Zurich.json",
+        sydney: "http://api.wunderground.com/api/c6a9124b70fbd2d2/geolookup/conditions/forecast/q/Australia/Sydney.json"
+        }
+    });
+})();
+
---
+++ Java Script/site.js
@@ -0,0 +1,43 @@
+﻿/// <reference path="global.js" />
+/// <reference path="contactForm.js" />
+
+(function () {
+    "use strict";
+    $(function () {
+
+        $(".overlay").hover(function () {
+
+            var rootElement = $(this);
+
+            var countryObj = window.entites.urlObjs[$(this).attr("id")];
+            console.log(countryObj);
+            $.ajax({
+
+                method: "GET",
+
+                url: countryObj,
+
+                cache: false,
+
+                dataType: "json",
+
+                error: function (err) {
+                    console.error("Error: " + err.status + ", " + err.statusText);
+                }
+                /*     function to read live feed of weather ftom API provided by the weather website . Each location have it own APi file , that been taken from global.js    */ 
+            }).then(function (response) {
+                console.log(rootElement);
+                $(rootElement).css("color", "white");
+                var spanInfo = "<h3>" + response.location.country_name + "</h3><p><span class='weatherSpan'>Current temperature: </span>" + response.current_observation.temp_c + "&#8451;</p><p><span class='weatherSpan'>Wind observation: </span>" + response.current_observation.wind_string + "</p><p><span class='weatherSpan'>Humidity: </span>" + response.current_observation.relative_humidity + "</p>" + "</p><p><span class='weatherSpan'>Weather: </span>" + response.current_observation.weather + "</p>"; //weather
+                $(rootElement).html(spanInfo);
+
+
+            });
+
+        });
+
+
+
+    });
+
+})();
